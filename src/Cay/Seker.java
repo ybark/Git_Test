63037595e74db462929d8e3fea6079dd11b7f98a
ybark <kevindoughlife.com>
2020-08-23 23:38:59 -0400

    Cift Seker

--- src/Cay/Seker.java
+++ src/Cay/Seker.java
@@ -3,8 +3,7 @@ package Cay;
 public class Seker {
 
     public static void main(String[] args) {
-        System.out.println("Tek Seker");
-        System.out.println("Tek Seker");
+        System.out.println("cift Seker");
     }
 
 }
